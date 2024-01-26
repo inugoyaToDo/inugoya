f714272e7bea53763d12383d0761cf1c75b89253
81901 <81901@mogmog17>
2024-01-26 21:28:11 +0900

    アプリケーション起動完了確認

--- todoapp/src/main/java/com/todo/controller/TodoController.java
+++ todoapp/src/main/java/com/todo/controller/TodoController.java
@@ -1,5 +1,26 @@
 package com.todo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.todo.entity.Todo;
+import com.todo.mapper.TodoMapper;
+
+@Controller
 public class TodoController {
+	
+	@Autowired
+	TodoMapper todoMapper;
+	
+	@RequestMapping(value="/")
+	public String index(Model model) {
+		List<Todo> list = todoMapper.selectAll();
+		model.addAttribute("todos", list);
+		return "index";
+	}
 
 }
--- todoapp/src/main/java/com/todo/mapper/TodoMapper.java
+++ todoapp/src/main/java/com/todo/mapper/TodoMapper.java
@@ -1,5 +1,14 @@
 package com.todo.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.todo.entity.Todo;
+
+@Mapper
 public interface TodoMapper {
+	
+	public List<Todo> selectAll();
 
 }
